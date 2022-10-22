f384e0d20169bd8551daeb3df5e6c02d47809214
LUIS210 <luissotavios@outlook.com>
2022-10-22 02:18:50 -0300

    feat: Adicionado Pattern Builder

--- src/main/java/com/escola/Nauticos/Aluno.java
+++ src/main/java/com/escola/Nauticos/Aluno.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class Aluno {
 
+    public Aluno(CPF cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
     private CPF cpf;
     private String nome;
     private Email email;
@@ -13,7 +19,7 @@ public class Aluno {
 
     public void adicionarTelefone (String ddd, String numero) {
 
-        this.telefoneList.add (new Telefone (ddd, numero));
+        this.telefoneList.add(new Telefone(ddd, numero));
 
     }
 }
